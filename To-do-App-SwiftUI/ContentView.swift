03c2640ceff935ab5d379802a0d1ac73cc1d816a
Vitor Ferraz Varela <vitorfvarela@gmail.com>
2020-05-13 09:24:34 -0300

    Initial Commit

---
+++ To-do-App-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-do-App-SwiftUI
+//
+//  Created by vitor.ferrazvarela on 13/05/20.
+//  Copyright © 2020 vitor.ferrazvarela. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
